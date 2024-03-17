797a415376a589a5914f3d725b6f4aa42f463865
ShadowScribe <bleeper.slushy-0a@icloud.com>
2024-03-17 20:51:23 +0000

    day-1

---
+++ src/day-1.rs
@@ -0,0 +1,25 @@
+fn main() {
+    // Declare and initialize variables
+    let num1 = 10;
+    let num2 = 5;
+
+    // Addition operation
+    let sum = num1 + num2;
+    println!("Sum: {}", sum);
+
+    // Subtraction operation
+    let difference = num1 - num2;
+    println!("Difference: {}", difference);
+
+    // Multiplication operation
+    let product = num1 * num2;
+    println!("Product: {}", product);
+
+    // Division operation
+    let quotient = num1 / num2;
+    println!("Quotient: {}", quotient);
+
+    // Remainder operation (modulus)
+    let remainder = num1 % num2;
+    println!("Remainder: {}", remainder);
+}
